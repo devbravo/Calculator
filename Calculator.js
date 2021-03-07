1ebbb045bb1bc3fbe5b4c8f766d827e33aab5d1b
Diego <diegofranco711@gmail.com>
2021-03-07 17:57:25 -0300

    Fixed the most important bugs and added a display for the last 10 calculations

--- Calculator.js
+++ Calculator.js
@@ -1,44 +1,109 @@
 class Calculator {
-  constructor(previousTextElement, currentTextElement) {
+  constructor(previousTextElement, currentTextElement, prevCalcTextElement) {
     this.previousTextElement = previousTextElement;
     this.currentTextElement = currentTextElement;
+    this.prevCalcTextElement = prevCalcTextElement;
+    this.array = [];
     this.clear();
   }
 
-  // clears the display after initiazion
+  // Clear the display and set currentOperation to 0
   clear() {
-    this.previousOperand = '';
-    this.currentOperand = '';
+    this.previousOperation = '';
+    this.currentOPeration = 0;
     this.operand = undefined;
+    this.array = [];
+    this.prevCalcTextElement.innerText = '';
   }
 
-  // Appends a number
+  // Add the last 10 calculations to an array for display
+  calcToArray() {
+    this.array.push(this.currentTextElement.innerText);
+    if (this.array.length > 10) {
+      this.array.shift();
+    }
+  }
+
+  // Append a number
   appendNumber(number) {
-    this.currentOperand = this.currentOperand.toString() + number.toString();
+    // this.currentOPeration =
+    //this.currentOPeration.toString() + number.toString();
+    if (this.currentTextElement.innerText === '0' && number === '0') {
+      this.currentOPeration = '0';
+    } else if (this.currentTextElement.innerText === '0' && number !== '0') {
+      this.currentOPeration = number.toString();
+      this.previousOperation = '';
+    } else if (this.previousOperation && this.currentOPeration > 0) {
+      this.previousOperation = '';
+      this.currentOPeration = number.toString();
+    } else {
+      this.currentOPeration =
+        this.currentOPeration.toString() + number.toString();
+    }
   }
 
-  // Choose an operand (+, -, *, /, =)
+  // add a operand (+, -, *, /, =)
   chooseOperand(operand) {
     this.operand = operand;
-    this.previousOperand = this.currentOperand;
-    this.currentOperand = '';
+    this.previousOperation = this.currentOPeration + this.operand;
+    this.currentOPeration = '';
   }
 
-  // Updates the display when clicking
+  // compute the actions
+  compute() {
+    let result;
+    const prev = parseFloat(this.previousOperation);
+    const current = parseFloat(this.currentOPeration);
+    switch (this.operand) {
+      case '+':
+        result = prev + current;
+        break;
+      case '-':
+        result = prev - current;
+        break;
+      case '*':
+        result = prev * current;
+        break;
+      case '÷':
+        result = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.previousOperation =
+      this.previousOperation + this.currentOPeration + '=';
+    this.currentOPeration = result;
+    this.operand = undefined;
+
+    // console.log(this.currentOPeration);
+  }
+
+  // Update the display after an action
   updateDisplay() {
-    this.currentTextElement.innerText = this.currentOperand;
-    this.previousTextElement.innerText = this.previousOperand;
+    this.currentTextElement.innerText = this.currentOPeration;
+    this.previousTextElement.innerText = this.previousOperation;
+  }
+
+  // Update the display for the previous 10 calculations
+  updatePrevCalcDisplay() {
+    this.prevCalcTextElement.innerText = this.array;
   }
 }
-const numberBtn = document.querySelectorAll('[data-number]');
-const operandBtn = document.querySelectorAll('[data-operand]');
-const clearBtn = document.querySelector('[data-clear]');
-const equalBtn = document.querySelector('[data-equal]');
-const previousTextElement = document.querySelector('[data-previous]');
-const currentTextElement = document.querySelector('[data-current]');
+numberBtn = document.querySelectorAll('[data-number');
+OperandBtn = document.querySelectorAll('[data-operand');
+equalBtn = document.querySelector('[data-equal');
+clearBtn = document.querySelector('[data-clear');
+previousTextElement = document.querySelector('[data-previous');
+currentTextElement = document.querySelector('[data-current');
+prevCalcTextElement = document.querySelector('[data-prev-10-calc]');
 
-const calculator = new Calculator(previousTextElement, currentTextElement);
+const calculator = new Calculator(
+  previousTextElement,
+  currentTextElement,
+  prevCalcTextElement
+);
 
+// Methods for adding number, choosing operands and computing
 numberBtn.forEach(button => {
   button.addEventListener('click', () => {
     calculator.appendNumber(button.innerText);
@@ -46,9 +111,21 @@ numberBtn.forEach(button => {
   });
 });
 
-operandBtn.forEach(button => {
+OperandBtn.forEach(button => {
   button.addEventListener('click', () => {
     calculator.chooseOperand(button.innerText);
     calculator.updateDisplay();
   });
+});
+
+equalBtn.addEventListener('click', () => {
+  calculator.compute();
+  calculator.updateDisplay();
+  calculator.calcToArray();
+  calculator.updatePrevCalcDisplay();
+});
+
+clearBtn.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
 });
